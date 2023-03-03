06905aefa2a8967fa3e4db77fb1f5ab8c1530ab7
akshaypaladi0304 <96440723+akshaypaladi0304@users.noreply.github.com>
2023-03-03 11:18:35 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(600, 600);
+    canvas.center();
+}
+
+function draw(){
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
